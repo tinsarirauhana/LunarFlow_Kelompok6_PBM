1ad012cad67e23a89e6489b035714db510cc6295
dian islami <dianislam1@mhs.usk.ac.id>
2026-06-24 14:55:08 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:intl/date_symbol_data_local.dart'; // tambah
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:device_preview/device_preview.dart';
 import 'core/theme/app_theme.dart';
 import 'pages/splash_page.dart';
 import 'core/config/supabase_config.dart';
@@ -8,14 +9,19 @@ import 'core/config/supabase_config.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await initializeDateFormatting('id_ID', null); // tambah
+  await initializeDateFormatting('id_ID', null);
 
   await Supabase.initialize(
     url: SupabaseConfig.url,
     anonKey: SupabaseConfig.anonKey,
   );
 
-  runApp(const LunarFlowApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const LunarFlowApp(),
+    ),
+  );
 }
 
 class LunarFlowApp extends StatelessWidget {
@@ -26,6 +32,9 @@ class LunarFlowApp extends StatelessWidget {
     return MaterialApp(
       title: 'LunarFlow',
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: AppTheme.theme,
       home: const SplashPage(),
     );
